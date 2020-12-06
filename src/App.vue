09fb01f567fc1a63e873bca38b1254319928c87a
Leonardo <leopiccionia@gmail.com>
2020-12-06 10:33:02 -0300

    Add bug reproduction

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <input type="text" v-model="foo">
+  <button type="button" @click="emitFoo">Click</button>
+</template>
+
+<script setup>
+  import { defineEmit, defineProps, ref } from 'vue'
+
+  const emit = defineEmit(['foo'])
+
+  const foo = ref('')
+
+  const emitFoo = () => {
+    console.log(`Emitting 'foo' with value '${foo.value}'`)
+    emit('foo', foo.value)
+  }
+</script>
